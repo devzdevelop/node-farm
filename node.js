886dd5aec80bbeaca637fccb4ffcb4e9cdde15cb
devzdevelop <devbusinessinfluencer@gmail.com>
2024-10-03 16:49:42 -0500

    inital commit

---
+++ node.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+////////////////////////////////////////////////////
+// FILES
+// blocking syncronous code
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/input.txt', textOut);
+// console.log('File has been written');
+
+// non-blocking asynchronous code
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+// 	if (err) return console.log('Error 💥');
+// 	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+// 		console.log(data2);
+// 		fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+// 			console.log(data3);
+
+// 			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+// 				console.log('Your file has been written');
+// 			});
+// 		});
+// 	});
+// });
+// console.log('Will read file!');
+
+////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+	const pathName = req.url;
+
+	if (pathName === '/' || pathName == '/overview') {
+		res.end('This is the OVERVIEW.');
+	} else if (pathName === '/product') {
+		res.end('This is the product.');
+	}
+});
+
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Listening to request on port 8000');
+});
