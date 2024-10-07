77598b8c7d7a377c4ebd7c1ddc2660e26131a56c
devzdevelop <devbusinessinfluencer@gmail.com>
2024-10-07 05:29:23 -0500

    more

--- index.js
+++ index.js
@@ -37,16 +37,16 @@ let count = 0;
 // Run Once
 
 const tempOverview = fs.readFileSync(
-	`${__dirname}/templates/template-overview.html`,
-	'utf-8'
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
-	`${__dirname}/templates/template-card.html`,
-	'utf-8'
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
-	`${__dirname}/templates/template-product.html`,
-	'utf-8'
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -58,44 +58,44 @@ console.log(slugify('Fresh Avocado', { lower: true }));
 
 // Always running
 const server = http.createServer((req, res) => {
-	// const pathname = req.url;
-	const { query, pathname } = url.parse(req.url, true);
-	// Overview page
-	if (pathname === '/' || pathname == '/overview') {
-		res.writeHead(200, { 'Content-type': 'text/html' });
+  // const pathname = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  // Overview page
+  if (pathname === '/' || pathname == '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-		const cardsHtml = dataObj
-			.map((el) => replaceTemplate(tempCard, el))
-			.join('');
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
 
-		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-		res.end(output);
+    res.end(output);
 
-		// Product page
-	} else if (pathname === '/product') {
-		res.writeHead(200, { 'Content-type': 'text/html' });
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-		let product = dataObj[query.id];
-		let output = replaceTemplate(tempProduct, product);
+    let product = dataObj[query.id];
+    let output = replaceTemplate(tempProduct, product);
 
-		res.end(output);
+    res.end(output);
 
-		// API
-	} else if (pathname === '/api') {
-		res.writeHead(200, { 'Content-type': 'application/json' });
-		res.end(data);
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
 
-		// Not found page
-	} else {
-		res.writeHead(404, {
-			'Content-type': 'text/html',
-			'my-own-header': 'hello-world',
-		});
-		res.end('<h1>Page not found!</h1>');
-	}
+    // Not found page
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-	console.log('Listening to request on port 8000');
+  console.log('Listening to request on port 8000');
 });
